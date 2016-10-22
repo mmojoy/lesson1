cce6bd81888c14cd19886df30a3f863421e816ec
mmojoy <mmojoy.net@gmail.com>
2016-10-22 17:27:47 +0300

    Add files via upload

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,40 @@
 require 'date'
 class Lesson1
-  def sum(val = 0)
-    # TODO
-  end
+	def sum(val = 0)
 
-  def age(birthday)
-    # TODO
-  end
+		val = val.to_s
 
-  def name
-    # TODO
-  end
+		if val.size == 1
+			val.to_i
+		else
+			val.split('').map(&:to_i).inject(:+)
+		end
+	end
+
+	def age(birthday)
+
+		date = DateTime.now
+		my_birthday	= Date.new
+		live_days = (my_birthday - date).to_i
+		seconds = (live_days*24*60*60).to_i
+		minutes = (seconds / 60).to_i
+		hours = (minutes / 60)
+		days = (hours / 24)
+		years = (days / 365)
+
+		puts "Я живу #{years} года или #{days} дней или #{hours} часов или #{minutes} минут или #{seconds} секунд"
+
+	end
+
+	def name
+
+		puts "Please enter your name:"
+		your_name = gets
+
+		full = " Hello" + your_name + "Super Man!"
+
+
+
+
+	end
 end
--- my_array.rb
+++ my_array.rb
@@ -1,63 +1,66 @@
 class MyArray
-  attr_accessor :array
+	attr_accessor :array
 
-  def initialize(arr = [])
-    @array = arr
-  end
+	def initialize(arr = [])
+		@array = arr
+	end
 
-  def size
-    # TODO
-  end
+	def size
+		array.size
+	end
 
-  def reverse
-    # TODO
-  end
+	def reverse
+		array.reverse
+	end
 
-  def max
-    # TODO
-  end
+	def max
+		array.max
+	end
 
-  def min
-    # TODO
-  end
+	def min
+		array.min
+	end
 
-  def desc
-    # TODO
-  end
+	def desc
+		array.sort
+	end
 
-  def asc
-    # TODO
-  end
+	def asc
+		array.sort.reverse
+	end
 
-  def odd
-    # TODO
-  end
+	def odd
+		array.find_all { |item| (item % 2).nonzero? }
+	end
 
-  def multiple_to_three
-    # TODO
-  end
+	def multiple_to_three
+		array.find_all	{ |item| (item % 3).zero? }
+	end
 
-  def uniq
-    # TODO
-  end
+	def uniq
+		array.uniq
+	end
 
-  def devide_on_ten
-    # TODO
-  end
+	def devide_on_ten
+		array.map { |item| item / 10.0 }
+	end
 
-  def chars
-    # TODO
-  end
+	def chars
+		abc = (:a..:z).to_a
+		abc.values_at(*@array)
+	end
 
-  def switch
-    # TODO
-  end
+	def switch
+		min = array.min
+		max = array.max
+		array[min], array[max] = array[max], array[min]
+	end
 
-  def before_min
-    # TODO
-  end
+	def before_min
+		array[0...array.index(array.min)]
+	end
 
-  def three_smallest
-    # TODO
-  end
+	def three_smallest
+		@array.sort[0..2]
+	end
 end
